520e270584e8d9b3a09c977a9a88b49dad9f3539
Patrick Jimison <patrick.jimison@altera.com>
2025-08-21 10:42:46 -0700

    Add game update clock

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True: 
         # Check if the user has closed the window and exit the game loop
         for event in pygame.event.get():
@@ -19,6 +22,9 @@ def main():
         
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        # Get milliseconds since last game update
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
